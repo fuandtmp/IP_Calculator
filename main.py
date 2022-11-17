575d668e41f616807e19eacd3952b7046b5d82e7
levus <levusachev082@outlook.com>
2022-11-17 20:28:07 +0400

    Prepare main py to bot for telegram and create ip_calc.py for more flexibility in future

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# Version 0.1.2
+# Version 0.2.0
+# In future this will be telegram bot
+import ip_calc
+
+# Get IP address and mask from user
+entered_address = input('Please, enter the address: ')
+entered_mask = input('Please, choose the mask: ')
+# To calculate entered IP and mask
+ip_calc.ip_calculator(entered_address, entered_mask)
