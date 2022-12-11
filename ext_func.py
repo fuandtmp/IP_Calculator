da35fc8bd2260f54f71edb112a98dbddec627c08
levus <levusachev082@outlook.com>
2022-12-11 23:33:19 +0300

    added function "to_check_ip_address" which to check ip address

--- ext_func.py
+++ ext_func.py
@@ -10,3 +10,20 @@ def to_revert_mask(mask_of_network):
             wild_mask += mask_of_network[i].join('1')
             i += 1
     return wild_mask
+
+
+def to_check_ip_address(ip_address):
+    if ip_address.strip().replace(".", "").isdigit():
+        split_address = ip_address.split(".")
+        first_octet = int(split_address[0])
+        i = 0
+        while i < 4:
+            if -1 < int(split_address[i]) < 256:
+                i += 1
+            else:
+                return False
+        if first_octet <= 0:
+            return False
+        return True
+    else:
+        return False
